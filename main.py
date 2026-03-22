3e16d09663b4deed7315922ad13c5162839a654e
Hannah <hannahdanvynguyen@gmail.com>
2026-03-21 23:21:03 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -5,16 +5,14 @@
 \margl1440\margr1440\vieww11520\viewh8400\viewkind0
 \pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
 
-\f0\fs24 \cf0 # Quant Finance Market Analyzer\
+\f0\fs24 \cf0 import yfinance as yf\
+import pandas as pd\
 \
-Python project that analyzes stock market data and calculates portfolio returns.\
+stocks = ["AAPL","NVDA","MSFT","SPY"]\
 \
-Features\
-- downloads historical stock data\
-- calculates annual returns\
-- compares multiple stocks\
+data = yf.download(stocks,start="2020-01-01")["Adj Close"]\
 \
-Technologies\
-Python\
-Pandas\
-yfinance}
+returns = data.pct_change().dropna()\
+\
+print("Average Annual Returns")\
+print(returns.mean()*252)}
